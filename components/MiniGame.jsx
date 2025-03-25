e63156934b49be58e0698573e7fbcf42b5d8fa03
Umar-Farooq-5757 <aamirgel17@gmail.com>
2025-03-25 23:18:42 +0500

    bug fixes

--- components/MiniGame.jsx
+++ components/MiniGame.jsx
@@ -37,7 +37,7 @@ const MiniGame = () => {
   };
   return (
     <main className="bg-slate-800 min-h-[100vh] text-white flex flex-col items-center justify-center gap-10">
-      <h1 className="uppercase text-3xl">
+      <h1 className="uppercase text-md sm:text-3xl">
         You've gotten{" "}
         <span className="rightAnswers text-red-500 font-bold">
           {noOfRightAnswers}
@@ -46,14 +46,13 @@ const MiniGame = () => {
       </h1>
       <section>
         <div
-          className="result bg-green-800 p-3 px-12 rounded-md text-3xl hidden"
+          className="result bg-green-800 p-2 px-6 sm:p-3 sm:px-12 rounded-md text-xl sm:text-3xl hidden"
           ref={correctAnswerBox}
         >
-          {/* You're right! {previousNo1} &times; {previousNo2} really is {previousNo1 * previousNo2} */}
           Correct!
         </div>
         <div
-          className="result bg-red-800 p-3 px-12 rounded-md text-3xl hidden"
+          className="result bg-red-800 p-2 px-6 sm:p-3 sm:px-12 rounded-md text-xl sm:text-3xl hidden"
           ref={wrongAnswerBox}
         >
           Wrong!
@@ -69,7 +68,7 @@ const MiniGame = () => {
       </section>
       <div
         onClick={checkAnswer}
-        className="check select-none border py-4 px-24 text-2xl cursor-pointer text-blue-500 font-bold rounded-tl-full rounded-br-full hover:rounded-tr-full hover:rounded-bl-full hover:rounded-tl-none hover:rounded-br-none transition-all duration-300"
+        className="check select-none border py-2 px-10 sm:py-4 sm:px-24 text-lg sm:text-2xl cursor-pointer text-blue-500 font-bold rounded-tl-full rounded-br-full hover:rounded-tr-full hover:rounded-bl-full hover:rounded-tl-none hover:rounded-br-none transition-all duration-300"
       >
         Check Your Answer
       </div>
